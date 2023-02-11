26afe6d71423650c82915056550528de88e0eb75
xuzhengzhou <2350122040@qq.com>
2023-02-11 19:10:52 +0800

    完成雷达UDP数据接入kafka

--- src/main/java/com/ocean/function/RadarDataSource.java
+++ src/main/java/com/ocean/function/RadarDataSource.java
@@ -1,47 +1,42 @@
 package com.ocean.function;
 
-import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.math3.random.RandomDataGenerator;
-import org.apache.flink.streaming.api.functions.source.datagen.RandomGenerator;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
 /**
  * @author 徐正洲
  * @create 2023-01-05 9:58
  * <p>
- * 雷达数据模拟
+ * 雷达数据UDP接收
  */
-public class RadarDataSource extends RandomGenerator<String> {
-    private RandomDataGenerator random = new RandomDataGenerator();
-    private JSONObject json = new JSONObject();
-
+public class RadarDataSource extends RichSourceFunction<String> {
+    private DatagramSocket datagramSocket = null;
 
     @Override
-    public String next() {
-        try {
-            json.put("TargetEchoAmplitude", random.nextInt(0, 255));
-            json.put("TargetDistance", random.nextInt(0, 12000));
-            json.put("TargetBearing", random.nextInt(0, 36000));
-            json.put("TargetElevation", random.nextInt(-100, 3000));
-            json.put("TargetXAxisDistance", random.nextInt(-12000, 12000));
-            json.put("TargetYAxisDistance", random.nextInt(-12000, 12000));
-            json.put("TargetZAxisDistance", random.nextInt(-12000, 12000));
-            json.put("PredictionDistance", random.nextInt(0, 12000));
-            json.put("PredictionBearing", random.nextInt(0, 36000));
-            json.put("PredictionElevation", random.nextInt(-100, 3000));
-            json.put("CourseOfTheTarget", random.nextInt(0, 36000));
-            json.put("TargetSpeed", random.nextInt(0, 2000));
-            json.put("TargetX-axisSpeed", random.nextInt(-2000, 2000));
-            json.put("TargetY-axisSpeed", random.nextInt(-2000, 2000));
-            json.put("TargetZ-axisSpeed", random.nextInt(-2000, 2000));
-            json.put("TargetLotNumber", random.nextInt(6000, 7999));
-            json.put("TrackingStatusMessage", random.nextInt(0, 4));
-            json.put("TargetType", "空飘物体待确认");
-            json.put("Ts", System.currentTimeMillis());
-            json.put("SourceType", "radar");
-            Thread.sleep(2000L);
-            return json.toJSONString();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+    public void open(Configuration parameters) throws Exception {
+        datagramSocket = new DatagramSocket(8899);
+    }
+
+    @Override
+    public void run(SourceContext<String> sourceContext) throws Exception {
+        while (true) {
+            //接收数据包
+            byte[] buffer = new byte[1024];
+
+            DatagramPacket datagramPacket = new DatagramPacket(buffer, 0, buffer.length);
+
+            datagramSocket.receive(datagramPacket);
+
+            String data = new String(datagramPacket.getData(), 0, datagramPacket.getLength());
+
+            sourceContext.collect(data);
         }
+    }
+
+    @Override
+    public void cancel() {
     }
 }
